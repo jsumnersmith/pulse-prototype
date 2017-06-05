7f55b4c741096e7a8c9e5fe1ec217ddce343fbdf
Joel Smith <jsumnersmith@gmail.com>
2017-06-05 13:31:39 -0400

    Removing service workers and reinstating routing.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import Page from './base/Page';
 
 ReactDOM.render(<Page />, document.getElementById('root'));
-registerServiceWorker();
