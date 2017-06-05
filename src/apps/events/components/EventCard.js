29244feb4e4a93d4f05e4fdb4020be8e0ad71fc3
Joel Smith <jsumnersmith@gmail.com>
2017-06-05 15:57:30 -0400

    Stashing before a big old eject

---
+++ src/apps/events/components/EventCard.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+import _ from 'lodash';
+import moment from 'moment';
+import ellipsis from 'text-ellipsis';
+
+class EventCard extends Component {
+  getAttendance() {
+    return _.filter(this.props.sampleEvent.attendees, attendee => attendee.confirmed).length || false;
+  }
+  render() {
+    const { sampleEvent, showAction, actionLink, actionTitle, isAttending, isAdmin, isList } = this.props;
+    return (
+      <div className="event-card">
+        <a href={this.props.eventLink || '#'}>
+          <div className="event-card-date">
+            <div className="event-card-date-month">
+              {moment(sampleEvent.date).format('MMM')}
+            </div>
+            <div className="event-card-date-day">{moment(sampleEvent.date).format('DD')}</div>
+            <div className="event-card-date-year">{moment(sampleEvent.date).format('YYYY')}</div>
+          </div>
+          <div className="event-card-header">
+            <h4 className="event-card-title"><strong>{sampleEvent.name}</strong></h4>
+            <h5 className="event-card-time">
+              <strong>{moment(sampleEvent.date).format('MMMM Do, YYYY')} {sampleEvent.startTime} - {sampleEvent.endTime}</strong>
+            </h5>
+          </div>
+          <div className="row event-card-content">
+            <div className="col-md-8">
+              <div className="col-md-12">
+                { sampleEvent.description ? <p className="event-card-description">{sampleEvent.description}</p> : null }
+              </div>
+              <h6 style={{marginTop: 0}} className="col-md-4"><i className="fa fa-map-marker circle-icon--medium"></i> <strong>{sampleEvent.location}</strong></h6>
+              <h6 style={{marginTop: 0}} className="col-md-4"><i className="fa fa-user circle-icon--medium"></i> <strong>{sampleEvent.leaders}</strong></h6>
+              <h6 style={{marginTop: 0}} className="col-md-4"><i className="fa fa-check circle-icon--medium"></i> <strong>{this.getAttendance()}/{sampleEvent.attendees.length}</strong> Attended</h6>
+            </div>
+            <div className="col-md-4">
+              { showAction ? (isAdmin ? <AdminControls /> : <TeacherControls isList={isList} isAttending={isAttending} actionLink={actionLink} actionTitle={actionTitle} />) : null }
+            </div>
+
+
+          </div>
+        </a>
+      </div>
+    );
+  }
+}
+
+class AdminControls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCode: false,
+    };
+  }
+  onClick() {
+    this.setState({ showCode: true });
+  }
+  render() {
+    return (
+      <div className="event-admin-control text-center">
+        <a href={'/events/edit.html'} className="btn btn-primary btn-block" style={{ marginLeft: 0 }}><i className="fa fa-pencil" /> Edit Event</a>
+        <a className="btn btn-default btn-block" style={{ marginLeft: 0, marginTop: 10, display: (!this.state.showCode ? 'block' : 'none') }} onClick={() => this.onClick()}>Generate Attendance Code</a>
+        <div className="event-attendance-code" style={{ marginTop: 10, display: (this.state.showCode ? 'block' : 'none') }}>X5fH8Gn</div>
+      </div>
+    );
+  }
+}
+
+const TeacherControls = ({ isAttending, isList, actionLink, actionTitle }) => (
+  <div className="event-teacher-control">
+    {
+      isAttending ?
+        <div>
+          { isList ?
+            <a href={actionLink} className="btn btn-primary">{actionTitle}</a>
+          :
+            <div style={{ marginBottom: 10 }}>
+              <label>Confirm Attendance</label>
+              <input className="form-control" placeholder="Enter code to confirm your attendance." />
+            </div>
+        }
+        <h4 className="green"><strong><i className="fa fa-check" /> {"You're Attending"}</strong></h4>
+        </div>
+      : <a href={actionLink} className="btn btn-primary">{actionTitle}</a>
+    }
+  </div>
+);
+
+export const QuickCard = ({ sampleEvent, isAttending, showAction = false, actionLink = '#actionLink', actionTitle = '', eventLink = '#' }) => (
+
+  <div className="event-card">
+    <a href={eventLink}>
+      <div className={`event-card-header ${showAction ? 'event-card-header--narrow' : ''}`}>
+        <h5 className="event-card-time">
+          <strong>{moment(sampleEvent.date).format('MMMM Do, YYYY')} {sampleEvent.startTime} - {sampleEvent.endTime}</strong>
+        </h5>
+        <h4 className="event-card-title"><strong>{sampleEvent.name}</strong></h4>
+      </div>
+      { showAction ? (<div className="event-card-actions"> { isAttending ? <h5 className="meta green" style={{ marginBottom: 0 }}><i className="fa fa-check" />{"You're Attending"}</h5> : <a href={actionLink} className="btn btn-primary btn-block" style={{ display: 'inline-block', width: 160 }}>{actionTitle}</a> } </div>) : null }
+      { sampleEvent.description ? <p>{ellipsis(sampleEvent.description, 200)}</p> : null }
+    </a>
+  </div>
+ );
+
+export default EventCard;
--- src/apps/events/index.js
+++ src/apps/events/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import SubHeader from './components/SubHeader';
+import EventSummaryList from './components/EventSummaryList';
+import sampleEvents from './components/sampleEvents';
 import './events.css';
 
 export default class Events extends Component {
@@ -7,6 +9,31 @@ export default class Events extends Component {
     return (
       <div className="wrapper">
         <SubHeader activeName="home" />
+        <h5 className="event-list-title" style={{background: "#007DA0"}}><i className="fa fa-calendar circle-icon--medium pulse-blue color-text"></i> <strong>Welcome!</strong></h5>
+        <div className="block-flat">
+          <div className="content">
+            <div className="row" style={{marginTop: 0}}>
+              <div className="col-md-12">
+                <span id="adminSearch"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row" style={{marginTop: 0}}>
+          <div className="col-md-6">
+            <h5 className="event-list-title" style={{background: "rgb(31, 175, 132)"}}><i className="fa fa-calendar-check-o circle-icon--medium green color-text"></i> <strong>Upcoming Events</strong></h5>
+            <EventSummaryList
+              index={4}
+              events={sampleEvents}
+              url={'/events/browse-upcoming.html'}
+              linkText={"Browse All Upcoming Events"}
+            />
+          </div>
+          <div className="col-md-6">
+            <h5 className="event-list-title" style={{background: "#8B698E"}}><i className="fa fa-check circle-icon--medium purple color-text"></i> <strong>Recent Events</strong></h5>
+            <div id="otherEventListMount"></div>
+          </div>
+        </div>
       </div>
     )
   }
