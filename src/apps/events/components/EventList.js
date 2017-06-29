d615bfbb5bfe3c1691a201978416ebdfc177b97b
Joel Smith <jsumnersmith@gmail.com>
2017-06-29 15:57:19 -0400

    Trying another approach.

--- src/apps/events/components/EventList.js
+++ src/apps/events/components/EventList.js
@@ -113,19 +113,17 @@ class EventList extends Component {
           }
         </div>
         <div className="col-md-4">
-          <div className="block-flat" style={{marginTop: 48, marginLeft: 40, borderTop: "3px solid #007DA0"}}>
+          <div className="block-flat text-center" style={{marginTop: 48, marginLeft: 40, borderTop: "3px solid #007DA0"}}>
+            <div style={{marginBottom: 10}}><label>Show Me events</label></div>
+            <div className="btn-group text-center" style={{width: "100%"}}>
+              <a className={`btn btn-${this.state.upcoming ? 'default' : 'primary'}`} style={{width: '48%'}} onClick={this.toggleUpcoming}><i className="fa fa-angle-left"/> Before</a>
+              <a className={`btn btn-${this.state.upcoming ? 'primary' : 'default'}`} style={{width: '48%'}} onClick={this.toggleUpcoming}>After <i className="fa fa-angle-right"/></a>
+            </div>
             <DayPicker
               onDayClick={this.onDayClick}
               selectedDays={[this.state.selectedDay]}
             />
-            <div style={{marginTop: 10, marginBottom: 5}}>
-              <label>Show Me events</label>
-              <select className="form-control" onChange={this.setUpcoming} style={{display: "inline-block", width: 100, marginLeft: 2, marginRight: 2}}>
-                <option value="true">After</option>
-                <option value="false">Before</option>
-              </select>
-              <label>this date</label>
-            </div>
+
           </div>
         </div>
       </div>
