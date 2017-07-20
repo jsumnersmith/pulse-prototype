f20dcad66d8ad754fa086697647f625b58adfc30
Joel Smith <jsumnersmith@gmail.com>
2017-07-20 09:05:45 -0400

    updated styles for buttons.

--- src/components/permissionButtons/components/BigButton.js
+++ src/components/permissionButtons/components/BigButton.js
@@ -19,7 +19,7 @@ export default class BigButton extends Component {
     const { isActive } = this.state;
     return (
       <div className={`btn-huge ${isActive ? "btn-huge__active" : ""}`} onClick={this.toggleActive}>
-        <i className={`fa ${iconClass} btn-huge__icon`} />
+        <i className={`fa ${isActive ? "fa-check-circle green" : "fa-circle-thin"} btn-huge__icon`} />
         <div className="btn-huge__content">
           <h4 className="btn-huge__title"><strong>{title}</strong></h4>
           <p className="btn-huge__description">{description}</p>
