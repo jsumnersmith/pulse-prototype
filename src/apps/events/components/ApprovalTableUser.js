867d5ebeba986ce999574a5b8014b0735558cc85
Joel Smith <jsumnersmith@gmail.com>
2017-11-02 16:22:42 -0400

    Final touches on user-submitted events

--- src/apps/events/components/ApprovalTableUser.js
+++ src/apps/events/components/ApprovalTableUser.js
@@ -21,50 +21,53 @@ export default class ApprovalTable extends Component {
   render(){
     const eventsToShow = this.state.show === 'active' ? [sampleSubmittedEvents[0]] : [sampleSubmittedEvents[1]]
     return (
-      <div>
+    <div>
       <h5 className="event-list-title" style={{background: "#8B698E"}}>
-          <div style={{display:"inline-block", width: "calc(100% - 400px)"}}>
+          <div style={{display:"inline-block", width: "calc(100% - 500px)"}}>
             <i className="fa fa-check circle-icon--medium purple color-text"></i>
             <strong>Submitted Events</strong>
           </div>
-          <span style={{display: "inline-block", width: 400}}>
-            <label style={{display: "inline-block", color: "white"}}>Show</label>
-            <span style={{display: "inline-block", width: 240, marginLeft: 10}}>
-              <select className="form-control" style={{background: 'white'}} onChange={this.updateShow}>
-                <option value="active">Active</option>
-                <option value="closed">Closed</option>
-              </select>
-            </span>
+          <span style={{display: "inline-block", width: 500}} className="text-right">
+            <Link to={"/events/submit"} className="btn btn-default">Submit an Event</Link>
           </span>
         </h5>
-        <table>
-          <thead className="no-border">
-            <tr>
-              <th><strong>Date Submitted</strong></th>
-              <th><strong>Event Title</strong></th>
-              <th className="text-center"><strong>Details</strong></th>
-              <th className="text-center"><strong>Status</strong></th>
-            </tr>
-          </thead>
-          <tbody className="no-border-y">
-            {
-              eventsToShow.map(submittedEvent => {
-                return (
-                  <tr>
-                    <td>{moment(submittedEvent.date).format('MMMM Do, YYYY')}</td>
-                    <td><strong>{submittedEvent.name}</strong></td>
-                    <td className="text-center">
-                      <Link to={`/events/submissions/${submittedEvent.id}`} className="btn btn-trans btn-primary btn-sm" ><i className="fa fa-info"></i> See Details</Link>
-                    </td>
-                    <td>
-                      <DisplayStatus status={`${this.state.show === 'active' ? 'pending' : 'closed'}`}/>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+        <div className="events-table-actions">
+          <label >Show</label>
+          <select className="form-control" style={{background: 'white', width: 150}} onChange={this.updateShow}>
+            <option value="active">Active</option>
+            <option value="closed">Closed</option>
+          </select>
+        </div>
+        <div style={{padding: 20, background: 'white'}}>
+          <table>
+            <thead className="no-border" style={{background: '#eee'}}>
+              <tr>
+                <th><strong>Date Submitted</strong></th>
+                <th><strong>Event Title</strong></th>
+                <th className="text-center"><strong>Details</strong></th>
+                <th><strong>Status</strong></th>
+              </tr>
+            </thead>
+            <tbody className="no-border-y" style={{background: 'white'}}>
+              {
+                eventsToShow.map(submittedEvent => {
+                  return (
+                    <tr>
+                      <td>{moment(submittedEvent.date).format('MMMM Do, YYYY')}</td>
+                      <td><strong>{submittedEvent.name}</strong></td>
+                      <td className="text-center">
+                        <Link to={`/events/submissions/${submittedEvent.id}`} className="btn btn-trans btn-primary btn-sm" ><i className="fa fa-info"></i> See Details</Link>
+                      </td>
+                      <td>
+                        <DisplayStatus status={`${this.state.show === 'active' ? 'pending' : 'closed'}`}/>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
