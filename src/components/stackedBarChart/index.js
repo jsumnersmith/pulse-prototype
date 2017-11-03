25a87826cd0d70a6111492ff43a13941e1a3441e
Joel Smith <jsumnersmith@gmail.com>
2017-11-03 13:44:20 -0400

    Updated stacked bar chart.

--- src/components/stackedBarChart/index.js
+++ src/components/stackedBarChart/index.js
@@ -60,9 +60,10 @@ export default () => (
             Blended Learning
           </div>
           <div className="stacked-bar-chart__bar-container">
-            <div className="stacked-bar-chart__bar blue" style={{width: "20%"}}></div>
-            <div className="stacked-bar-chart__bar purple" style={{width: "30%"}}></div>
-            <div className="stacked-bar-chart__bar red" style={{width: "30%"}}></div>
+            <div className="stacked-bar-chart__bar blue" style={{width: "20%"}}><span className="stacked-bar-chart__bar-segment">20</span></div>
+            <div className="stacked-bar-chart__bar purple" style={{width: "30%"}}><span className="stacked-bar-chart__bar-segment">30</span></div>
+            <div className="stacked-bar-chart__bar red" style={{width: "20%"}}><span className="stacked-bar-chart__bar-segment">20</span></div>
+            <div className="stacked-bar-chart__bar green" style={{width: "10%"}}><span className="stacked-bar-chart__bar-segment">10</span></div>
             <div className="stacked-bar-chart__count">80 hours</div>
           </div>
         </div>
@@ -71,9 +72,11 @@ export default () => (
               Technology Integration
             </div>
             <div className="stacked-bar-chart__bar-container">
-            <div className="stacked-bar-chart__bar blue" style={{width: "10%"}}></div>
-            <div className="stacked-bar-chart__bar purple" style={{width: "30%"}}></div>
-            <div className="stacked-bar-chart__bar red" style={{width: "20%"}}></div>
+            <div className="stacked-bar-chart__bar blue" style={{width: "10%"}}><span className="stacked-bar-chart__bar-segment">10</span></div>
+            <div className="stacked-bar-chart__bar purple" style={{width: "10%"}}><span className="stacked-bar-chart__bar-segment">10</span></div>
+            <div className="stacked-bar-chart__bar red" style={{width: "20%"}}><span className="stacked-bar-chart__bar-segment">20</span></div>
+            <div className="stacked-bar-chart__bar yellow" style={{width: "10%"}}><span className="stacked-bar-chart__bar-segment">10</span></div>
+            <div className="stacked-bar-chart__bar green" style={{width: "10%"}}><span className="stacked-bar-chart__bar-segment">10</span></div>
             <div className="stacked-bar-chart__count">60 hours</div>
             </div>
           </div>
@@ -87,9 +90,18 @@ export default () => (
             <div className="stacked-bar-chart__key-color purple"></div>
             <div className="stacked-bar-chart__key-label">3rd Grade</div>
           </div>
+
           <div className="stacked-bar-chart__key-item">
             <div className="stacked-bar-chart__key-color red"></div>
             <div className="stacked-bar-chart__key-label">4th Grade</div>
+          </div>
+          <div className="stacked-bar-chart__key-item">
+            <div className="stacked-bar-chart__key-color yellow"></div>
+            <div className="stacked-bar-chart__key-label">5th Grade</div>
+          </div>
+          <div className="stacked-bar-chart__key-item">
+            <div className="stacked-bar-chart__key-color green"></div>
+            <div className="stacked-bar-chart__key-label">6th Grade</div>
           </div>
         </div>
     </div>
