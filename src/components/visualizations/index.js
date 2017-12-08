b1c7cb0e899a9ed714988bc2daf9788c789b4cbe
Joel Smith <jsumnersmith@gmail.com>
2017-12-08 14:00:39 -0500

    Removed unused visualization.

--- src/components/visualizations/index.js
+++ src/components/visualizations/index.js
@@ -6,9 +6,6 @@ import GoalsSummary from './GoalsSummary';
 export default () => (
   <div className="wrapper">
     <h3 style={{marginBottom: 30}}><i className="fa fa-line-chart circle-icon green"/> <strong>Visualizations</strong></h3>
-      <GoalsSummary />
-    <div className="block-flat">
-      <CohortAnalysis />
-    </div>
+    <GoalsSummary />
   </div>
 )
