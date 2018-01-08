3933fa35b1035ca7c200862a23c37e6af3f40b9f
Joel Smith <jsumnersmith@gmail.com>
2018-01-08 10:24:47 -0500

    Added printable signup sheet

---
+++ src/apps/events/admin/SignupSheet.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default () => (
+  <div className="wrapper">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Attendance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Joel Smith</td>
+          <td>joel@kickup.org</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Bella Smith</td>
+          <td>bella@kickup.org</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Reggie Kinzig</td>
+          <td>reggie@kickup.org</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
