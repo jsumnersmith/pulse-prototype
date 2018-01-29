6341ffc893b7e50f0eeb41857e1d4a0fc821fb8b
Nicholas Smith <nicholasparkersmith@gmail.com>
2018-01-29 14:48:29 -0500

    Small changes to the queue view

--- src/apps/manageDataSources/Queue.js
+++ src/apps/manageDataSources/Queue.js
@@ -17,16 +17,29 @@ export default () => (
     </div>
     <div className="block-flat">
       <div className="content">
-        <h4 className="import-queue__title">Import Queue</h4>
+        <h4 className="import-queue__title">
+          Import Queue
+          <button
+            style={{
+              float: "right",
+              marginTop: "-3px",
+            }}
+            className="btn btn-primary btn-trans btn-sm"
+          >View All Tasks</button>
+        </h4>
         <div className="import-queue__wrapper">
           <table className="no-border">
             <thead className="no-border">
               <tr>
                 <th><strong>Queue Item</strong></th>
+                <th><strong>Task Type</strong></th>
                 <th><strong>Active</strong></th>
-                <th><strong>Last Runtime</strong></th>
-                <th><strong>Last Runtime Duration</strong></th>
+                <th><strong>Worker</strong></th>
+                <th><strong>Time Scheduled</strong></th>
+                <th><strong>Last Import</strong></th>
+                <th><strong>Last Import Runtime Duration</strong></th>
                 <th><strong>Progress</strong></th>
+                <th><strong>Actions</strong></th>
               </tr>
             </thead>
             <tbody className="no-border-y">
@@ -35,12 +48,21 @@ export default () => (
                   <tr>
                     <td>
                       <div className="import-queue__item-header">
-                        <span className={`meta circle-icon--small ${index === 0 && 'meeting-goals text-white'}`}><strong>{index + 1}</strong></span>
+                        <span className={`meta circle-icon--small ${[0,1].includes(index) && 'meeting-goals text-white'}`}><strong>{index + 1}</strong></span>
                         <h5><strong>{item.name}</strong> - {item.district}</h5>
                       </div>
                     </td>
                     <td>
-                      {index === 0 && <i className="fa fa-check green"/> }
+                      {item.type}
+                    </td>
+                    <td>
+                      {[0,1].includes(index) && <i className="fa fa-check green"/> }
+                    </td>
+                    <td>
+                      {item.worker}
+                    </td>
+                    <td>
+                      {item.timeScheduled}
                     </td>
                     <td>
                       {item.lastImport}
@@ -50,6 +72,9 @@ export default () => (
                     </td>
                     <td>
                       <code>{index === 0 ? 34 : 0 }/{item.submissionCount}</code>
+                    </td>
+                    <td>
+                      <button className="btn btn-primary">Cancel</button>
                     </td>
                   </tr>
                 ))
@@ -70,7 +95,10 @@ const sampleImportQueue = [
     importConfiguration: 'Regular',
     lastImport: '2017-12-11T14:30:00',
     lastImportDuration: '2 min',
-    importConfigurationExpiration:' 2018-2-16T14:30:00'
+    importConfigurationExpiration:' 2018-2-16T14:30:00',
+    timeScheduled: '2017-12-11T13:30:00',
+    type: 'Import survey',
+    worker: 'djangoweb1',
   },
   {
     name: 'PD Feedback Surveys',
@@ -78,7 +106,10 @@ const sampleImportQueue = [
     submissionCount: 114,
     lastImport: '2017-12-11T04:30:00',
     lastImportDuration: '15 sec',
-    importConfiguration: 'Nightly'
+    importConfiguration: 'Nightly',
+    timeScheduled: '2017-12-11T13:45:00',
+    type: 'Re-run processors',
+    worker: 'djangoweb2',
   },
   {
     name: 'Teacher Feedback Surveys',
@@ -86,6 +117,9 @@ const sampleImportQueue = [
     submissionCount: 452,
     lastImport: '2017-12-07T11:30:00',
     lastImportDuration: '4 minutes',
-    importConfiguration: 'Nightly'
+    importConfiguration: 'Nightly',
+    timeScheduled: '2017-12-11T14:30:00',
+    type: 'Delete derived data',
+    worker: 'djangoweb1',
   }
 ]
