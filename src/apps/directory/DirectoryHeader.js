1a665e9c4403d1d904b08250e210b05ec7f17c1c
Joel Smith <jsumnersmith@gmail.com>
2018-05-04 21:03:54 -0400

    Updated header to include addative actions

--- src/apps/directory/DirectoryHeader.js
+++ src/apps/directory/DirectoryHeader.js
@@ -8,10 +8,17 @@ export default () => (
       <img src={addressBookIcon} alt="Address Book" style={{height: 60, width: 'auto'}}/> <strong>Directory</strong>
     </h2>
     <nav className="directory-header__nav">
-      <Link className="directory-header__link meta" to={'/directory/'}>Directory</Link>
-      <Link className="directory-header__link meta" to={`/directory/entities`}>Entities</Link>
+      <Link className="directory-header__link meta" to={'/directory/'}>People</Link>
+      <Link className="directory-header__link meta" to={`/directory/entities`}>Other</Link>
       <Link className="directory-header__link meta" to={`/directory/groups`}>Groups</Link>
-      <button className="btn btn-primary btn-flat directory-header__link">Add Person</button>
+      <div className="btn-group" style={{marginLeft: 15}}>
+        <button className="btn btn-primary btn-flat directory-header__link" data-toggle="dropdown">Add People <i className="fa fa-caret-down" /></button>
+        <ul className="dropdown-menu">
+          <li><a><i className="fa fa-user circle-icon--small circle-icon--no-border" style={{marginRight: 5}}/> <strong>Add Person</strong></a></li>
+          <li><a><i className="fa fa-group circle-icon--small circle-icon--no-border" style={{marginRight: 5}}/> <strong>Add Group</strong></a></li>
+          <li><a><i className="fa fa-upload circle-icon--small circle-icon--no-border" style={{marginRight: 5}}/> <strong>Upload CSV</strong></a></li>
+        </ul>
+      </div>
     </nav>
   </header>
 );
