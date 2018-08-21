b4db42e303a9447253d25e40222e7f5db3fdbea2
Joel Smith <jsumnersmith@gmail.com>
2018-08-21 16:20:52 -0400

    Updated logic to only show view in certain situations

--- src/components/sharing/FullSharing.js
+++ src/components/sharing/FullSharing.js
@@ -31,48 +31,17 @@ export default class Sharing extends Component {
         <div className="block-flat">
           <div className="row">
             <div className="col-md-12">
-              <label>Make this event visible to:</label>
-              <button className={`btn btn-success ${!this.state.public && "btn-trans"} btn-xs`} onClick={()=>this.setPublicity(true)}>All Users</button>
-              <button className={`btn btn-success ${this.state.public && "btn-trans"} btn-xs`} onClick={()=>this.setPublicity(false)}>Only share with users and groups specified below</button>
+              <h3 style={{display:'flex', alignItems: 'center', marginTop: 0 }}><i className="far fa-users green circle-icon" style={{display: 'inline-flex', marginRight: 10}}/> <strong>Visibility and Permissions</strong></h3>
             </div>
             <div className="col-md-12">
-              <div className="sharing-header" style={{marginTop: 20}}>
-                <h5 className="sharing-header--title"><i className="far fa-users circle-icon--small" /> <strong>People</strong></h5>
-                <SearchInput placeholder="Search users or groups to give them access to this event." />
-                <table className="no-border">
-                  <thead className="no-border">
-                    <tr>
-                      <th><strong>User/Group</strong></th>
-                      <th style={{width: 250}}><strong>Access</strong></th>
-                      <th style={{width: 30}}></th>
-                    </tr>
-                  </thead>
-                  <tbody className="no-border-y">
-                    {
-                      this.state.groups.map(group =>
-                        <ShareRow
-                          item={group}
-                          rowType='group'
-                          itemIsPublic={this.state.public}
-                          removeUserOrGroup={this.removeUserOrGroup}
-                        />
-                      )
-                    }
-                    {
-                      this.state.users.map((user, index) =>
-                        <ShareRow
-                          item={user}
-                          rowType='user'
-                          itemIsPublic={this.state.public}
-                          removeUserOrGroup={this.removeUserOrGroup}
-                        />
-                      )
-                    }
-
-                  </tbody>
-                </table>.
-                <div className="text-center">{this.state.public && <label>*This event is currently shared with all users.</label>}</div>
+              <label>Who should see this event?</label>
+              <div>
+                <button className={`btn btn-success ${!this.state.public && "btn-trans"} btn-sm`} onClick={()=>this.setPublicity(true)}>All Users</button>
+                <button className={`btn btn-success ${this.state.public && "btn-trans"} btn-sm`} onClick={()=>this.setPublicity(false)}>Only share with users and groups specified below</button>
               </div>
+            </div>
+            <div className="col-md-12">
+              <ListView isPublic={this.state.public}/>
             </div>
           </div>
         </div>
@@ -81,41 +50,129 @@ export default class Sharing extends Component {
   }
 }
 
+class ListView extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      users: this.assignPermissions(users.slice(0,4)),
+      groups: this.assignPermissions(groups.slice(0,2))
+    }
+    this.setPermission = this.setPermission.bind(this);
+    this.removeUserOrGroup = this.removeUserOrGroup.bind(this);
+  }
+  assignPermissions(items){
+    let itemsClone =  Array.from(items);
+    itemsClone.map(item => item.id % 2 === 0 ? item.itemPermission = 'view' : item.itemPermission = 'edit')
+    return itemsClone;
+  }
+  setPermission(itemType, id, permission){
+    console.log(itemType);
+    let itemsClone = Array.from(this.state[itemType]);
+    itemsClone.map(item => {
+      if (id === item.id){
+        item.itemPermission = permission;
+      }
+    })
+    let state = {};
+    state[itemType] = itemsClone;
+    this.setState(state);
+  }
+  removeUserOrGroup(name){
+    console.log(name)
+    let updatedUsers = this.state.users.filter(user => user.name !== name);
+    let updatedGroups = this.state.groups.filter(group => group.name !== name);
+    this.setState({users: updatedUsers, groups: updatedGroups});
+  }
+  render(){
+    const {isPublic} = this.props;
+    return (
+      <div className="sharing-header" style={{marginTop: 20}}>
+        <h5 className="sharing-header--title"><i className="far fa-users circle-icon--small" /> <strong>Select people to set { !isPublic && "visibilty and"} permissions</strong></h5>
+        <SearchInput placeholder="Search users or groups to give them access to this event." />
+        <table className="no-border">
+          <thead className="no-border">
+            <tr>
+              <th><strong>User/Group</strong></th>
+              <th style={{width: 250}}><strong>Access</strong></th>
+              <th style={{width: 30}}></th>
+            </tr>
+          </thead>
+          <tbody className="no-border-y">
+            {
+              this.state.groups.map(group =>
+                <ShareRow
+                  item={group}
+                  rowType='groups'
+                  itemIsPublic={isPublic}
+                  setPermission={this.setPermission}
+                  removeUserOrGroup={this.removeUserOrGroup}
+                />
+              )
+            }
+            {
+              this.state.users.map((user, index) =>
+                <ShareRow
+                  item={user}
+                  rowType='users'
+                  itemIsPublic={isPublic}
+                  setPermission={this.setPermission}
+                  removeUserOrGroup={this.removeUserOrGroup}
+                />
+              )
+            }
+
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+
 class ShareRow extends Component {
   constructor(props){
     super(props);
     this.state = {
-      permission: 'view'
+      permission: 'edit'
     }
     this.setPermission = this.setPermission.bind(this);
-    this.showAsterisk = this.showAsterisk.bind(this);
+    this.showRow = this.showRow.bind(this);
   }
   setPermission(permission){
-    this.setState({permission});
+    const {item, rowType} = this.props;
+    this.props.setPermission(rowType, item.id, permission);
   }
-  showAsterisk(){
-    return this.props.itemIsPublic && this.state.permission === 'view';
+  showRow(){
+    const {item, itemIsPublic} = this.props;
+    if (itemIsPublic && item.itemPermission == 'view') {
+      return false;
+    } else {
+      return true;
+    }
   }
   render(){
-    const {item, rowType} = this.props;
+    const {item, rowType, itemIsPublic} = this.props;
     return (
-      <tr>
-        <td><Avatar rowType={rowType} /> <strong>{item.name}</strong></td>
-        <td>
-          <UserActions
-            onChange={(e)=>this.setPermission(e.target.value)}
-            showAsterisk={this.showAsterisk()}
-          />
-        </td>
-        <td><button className="btn btn-sm btn-danger btn-trans" onClick={() => this.props.removeUserOrGroup(item.name)}><i className="far fa-times" />Remove</button></td>
-      </tr>
+
+        this.showRow() &&
+          <tr>
+            <td><Avatar rowType={rowType} /> <strong>{item.name}</strong></td>
+            <td>
+              <UserActions
+                itemPermission={item.itemPermission}
+                onChange={(e)=>this.setPermission(e.target.value)}
+                itemIsPublic={itemIsPublic}
+              />
+            </td>
+            <td><button className="btn btn-sm btn-danger btn-trans" onClick={() => this.props.removeUserOrGroup(item.name)}><i className="far fa-times" />Remove</button></td>
+          </tr>
     )
   }
 }
 
 const Avatar = ({rowType}) => (
   <span>
-    {rowType === 'user'
+    {rowType === 'users'
       ?
       <img src={coffee} className="sharing-user-list--avatar" alt="Coffee Avatar"/>
       :
@@ -124,13 +181,13 @@ const Avatar = ({rowType}) => (
   </span>
 )
 
-const UserActions = ({onChange, showAsterisk}) => (
+const UserActions = ({itemPermission, onChange, itemIsPublic}) => (
   <div className="sharing-control-wrapper">
     <select className="form-control sharing-control" onChange={onChange}>
-      <option value="view" selected>Can View</option>
-      <option value="manage">Can Manage Attendance</option>
-      <option value="edit">Can Edit</option>
+      {itemIsPublic ? <option value="view" disabled>View</option> :
+      <option value="view" selected={itemPermission === 'view' ? true : false}>View</option> }
+      <option value="manage" selected={itemPermission === 'manage' ? true : false}>View and Manage Attendance</option>
+      <option value="edit" selected={itemPermission === 'edit' ? true : false}>View, Manage, and Edit</option>
     </select>
-    {showAsterisk && <label>*</label>}
   </div>
 )
