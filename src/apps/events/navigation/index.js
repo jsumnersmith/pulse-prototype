d5bd1e0b166aad438ef6de7717c3bdb71c86c7b8
Joel Smith <jsumnersmith@gmail.com>
2019-01-29 12:46:59 -0500

    Updated highlights and added stubbed in breadcrumb

--- src/apps/events/navigation/index.js
+++ src/apps/events/navigation/index.js
@@ -3,6 +3,12 @@ import SubHeader from '../components/SubHeader'
 
 export default () => (
   <div className="wrapper">
+    <div>
+      <span><i className="far fa-home"/> </span>
+      /
+      <span className="meta"> Events</span>
+    </div>
     <SubHeader activeName="browse" admin={false}/>
+    <div className="block-flat"></div>
   </div>
 )
