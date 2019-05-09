d5af9f4c02e3fb1b6f815c50db21a351655e3eb1
Joel Smith <jsumnersmith@gmail.com>
2019-05-09 10:21:02 -0400

    Added basic onboarding.

--- src/apps/onboarding/index.js
+++ src/apps/onboarding/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './onboarding.less';
-import logo from '../../images/logo.png';
+import logo from './logo.svg';
 
 import { TextInput  } from '../../componentLibrary/input';
 import  { Label, Paragraph } from '../../componentLibrary/text';
@@ -8,8 +8,8 @@ import  { Button } from '../../componentLibrary/button';
 
 export default () => (
   <div>
-    <div className="wrapper" style={{maxWidth: 600}}>
-      <div className="text-center" style={{marginBottom: 20}}><img src={logo} alt="Kickup Logo" /></div>
+    <div className="wrapper" style={{maxWidth: 500}}>
+      <div className="text-center" style={{marginBottom: 40}}><img src={logo} alt="Kickup Logo" style={{width: 200, height: 'auto'}}/></div>
 
       <Paragraph mb={3}>Welcome! You've been invited to join the <strong>--district_name--</strong> organization. The first step is to verify the email that you would like to use to access our platform.</Paragraph>
 
