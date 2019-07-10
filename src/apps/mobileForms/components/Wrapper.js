2a687610d2c123550052905092e55c896513b194
Joel Smith <jsumnersmith@gmail.com>
2019-07-10 15:36:15 -0400

    Improved mobile fit.

--- src/apps/mobileForms/components/Wrapper.js
+++ src/apps/mobileForms/components/Wrapper.js
@@ -14,14 +14,27 @@ const Wrapper = styled.div`
   );
   background-size: 100% auto;
   background-color: ${({theme}) => theme.colors.blue};
+  @media (max-width: 500px) {
+    background-image: linear-gradient(
+      20deg,
+      ${({theme}) => theme.colors.orange},
+      ${({theme}) => theme.colors.gold},
+      ${({theme}) => theme.colors.teal}
+    );
+  }
 `;
 
 const Inner = styled.div`
   width: 760px;
-  max-width: 95%;
+  max-width: 97%;
   padding: ${({theme})=> theme.space[5]}px;
   margin: ${({theme})=>theme.space[7]}px auto;
   background: ${({theme})=> theme.colors.white};
+  @media (max-width: 500px) {
+    padding-left: 0;
+    padding-right: 0;
+    margin: 1.5vw auto;
+  }
 `;
 
 export default ({children, ...props}) => (
